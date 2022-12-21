79cb08be953133902dac838494111e72e1eae60a
IAMTorres <82275287+IAMTorres@users.noreply.github.com>
2022-12-21 16:55:00 +0000

    Initial XML
    XML login and register

---
+++ app/src/main/java/pt/ipca/pa/RegisterActivity.kt
@@ -0,0 +1,11 @@
+package pt.ipca.pa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
