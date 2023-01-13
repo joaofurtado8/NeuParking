43cacc5db6245fd669615674e1ae4fd48ace7850
João Furtado <joaofurtado1889@gmail.com>
2023-01-13 14:23:13 +0000

    Payment Activity and XML

---
+++ app/src/main/java/pt/ipca/pa/Payment/PaymentActivity.kt
@@ -0,0 +1,12 @@
+package pt.ipca.pa.Payment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import pt.ipca.pa.R
+
+class PaymentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment)
+    }
+}
