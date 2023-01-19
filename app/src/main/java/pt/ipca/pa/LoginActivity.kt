7c5bc9bab51262b2553baf12ce4e29c9a8cdaac1
Rúben Gomes <75390587+rubenfsgomes@users.noreply.github.com>
2023-01-19 22:19:22 +0000

    Confirm reservation

--- app/src/main/java/pt/ipca/pa/LoginActivity.kt
+++ app/src/main/java/pt/ipca/pa/LoginActivity.kt
@@ -9,7 +9,6 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ProgressBar
 import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.auth0.jwt.JWT
